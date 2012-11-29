d817432715adc185c75c49c221de4d5b1fa501b9
Brian Roberg <robergb@dm.org>
2012-11-29 11:15:14 -0500

    Added proof of concept using custom ImportObject class.

--- dm/site_import/browser/views.py
+++ dm/site_import/browser/views.py
@@ -8,8 +8,16 @@ class DMSiteImportView(BrowserView):
     print 'Hello, world!'
     site = 'www.dm.org'
     page = '/site-homepage'
-    return self.get_cooked_body(site, page)
+    obj = ImportObject(site, page)
+    return obj.body
 	
+
+
+class ImportObject:
+
+  def __init__(self, site, page):
+    self.body = self.get_cooked_body(site, page)
+    
   def get_cooked_body(self, site, page):
     conn = httplib.HTTPConnection(site)
     conn.request('GET', "%s/CookedBody" % page)
