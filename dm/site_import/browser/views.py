7f121fa7e54704d1ff0d46278e4836a78e5216c8
Brian Roberg <robergb@dm.org>
2012-11-29 11:02:13 -0500

    Simple proof of concept using httplib.

--- dm/site_import/browser/views.py
+++ dm/site_import/browser/views.py
@@ -1,9 +1,17 @@
+from bs4 import BeautifulSoup
 from Products.Five.browser import BrowserView
+import httplib
 
 class DMSiteImportView(BrowserView):
 
   def __call__(self):
     print 'Hello, world!'
-    print "title = %s" % self.context.Title()
+    site = 'www.dm.org'
+    page = '/site-homepage'
+    return self.get_cooked_body(site, page)
 	
-
+  def get_cooked_body(self, site, page):
+    conn = httplib.HTTPConnection(site)
+    conn.request('GET', "%s/CookedBody" % page)
+    r = conn.getresponse()
+    return r.read()
