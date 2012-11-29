bfe5aa135efbca98969aedd3e0e37cc7ad338260
Brian Roberg <robergb@dm.org>
2012-11-29 11:56:27 -0500

    Moved httplib code into the DMSiteImportView object and created initial versions of is_already_retrieved and make_http_request methods.

--- dm/site_import/browser/views.py
+++ dm/site_import/browser/views.py
@@ -5,28 +5,39 @@ import httplib
 class DMSiteImportView(BrowserView):
 
   def __call__(self):
-    print 'Hello, world!'
-    site = 'www.dm.org'
-    page = '/foobar-homepage'
-    obj = ImportObject(site, page)
-    return obj.body
+
+    # Dictionary to keep track of what objects we've already seen.
+    # Each value is an Import Object.
+    # Each key is the object's absolute_url (as returned in the
+    # original site).
+    self.objects_seen = {}
+
+    self.conn = httplib.HTTPConnection('www.dm.org')
+    page = '/site-homepage'
+    return "Return value is %s." % self.is_already_retrieved('', page)
+
+  def is_already_retrieved(self, base_url, page):
+    url = self.make_http_request(base_url, page, 'absolute_url')
+    if url in self.objects_seen.keys():
+      return True
+    else:
+      return False
+    
+  def make_http_request(self, base_url, page, suffix=""):
+    self.conn.request('GET', "%s/%s" % (page, suffix))
+    r = self.conn.getresponse()
+    if r.status == 404:
+      msg = "Server at %s returned 404 for page %s" % (site, page)
+      raise NotFoundError, msg
+    return r.read()
 	
 
 
 class ImportObject:
 
-  def __init__(self, site, page):
-    self.body = self.get_cooked_body(site, page)
+  def __init__(self, absolute_url):
+    self.absolute_url = absolute_url
     
-  def get_cooked_body(self, site, page):
-    conn = httplib.HTTPConnection(site)
-    conn.request('GET', "%s/CookedBody" % page)
-    r = conn.getresponse()
-    if r.status == 404:
-      msg = "Server at %s returned 404 for page %s" % (site, page)
-      raise NotFoundError, msg
-    return r.read()
-
 
 class NotFoundError(Exception):
   pass
