bf10cb4ee6c5c93d438038a1459101511887c898
Brian Roberg <robergb@dm.org>
2012-12-06 16:13:23 -0500

    Extended refactoring to all ImportObject subclasses.

--- dm/site_import/browser/ImportObject.py
+++ dm/site_import/browser/ImportObject.py
@@ -13,6 +13,7 @@ class ImportObject(object):
     self.site_obj = self.view_obj.context[self.remote_obj.shortname]
     self.site_obj.setTitle(self.remote_obj.title)
 
+
 class ImportFile(ImportObject):
 
   def __init__(self, remote_obj, view_obj):
@@ -27,30 +28,34 @@ class ImportFile(ImportObject):
 
 class ImportFolder(ImportObject):
 
+  def __init__(self, remote_obj, view_obj):
+    self.type_name = 'Folder'
+    ImportObject.__init__(self, remote_obj, view_obj)
+
   def create(self):
-    print "Running create() for ImportFolder %s" % self.absolute_url
-    self.view_obj.context.invokeFactory('Folder', self.remote_obj.shortname)
-    obj = self.view_obj.context[self.remote_obj.shortname]
-    obj.setTitle(self.remote_obj.title)
-    obj.reindexObject()
+    ImportObject.create(self)
+    self.site_obj.reindexObject()
+
 
 class ImportImage(ImportObject):
 
+  def __init__(self, remote_obj, view_obj):
+    self.type_name = 'Image'
+    ImportObject.__init__(self, remote_obj, view_obj)
+
   def create(self):
-    print "Running create() for ImportImage %s" % self.absolute_url
-    self.view_obj.context.invokeFactory('Image', self.remote_obj.shortname)
-    obj = self.view_obj.context[self.remote_obj.shortname]
-    obj.setTitle(self.remote_obj.title)
-    obj.setImage(self.remote_obj.image)
-    obj.reindexObject()
+    ImportObject.create(self)
+    self.site_obj.setImage(self.remote_obj.image)
+    self.site_obj.reindexObject()
 
 
 class ImportPage(ImportObject):
 
+  def __init__(self, remote_obj, view_obj):
+    self.type_name = 'Document'
+    ImportObject.__init__(self, remote_obj, view_obj)
+
   def create(self):
-    print "Running create() for ImportPage %s" % self.absolute_url
-    self.view_obj.context.invokeFactory('Document', self.remote_obj.shortname)
-    obj = self.view_obj.context[self.remote_obj.shortname]
-    obj.setTitle(self.remote_obj.title)
-    obj.setText(self.remote_obj.get_cooked_body())
-    obj.reindexObject()
+    ImportObject.create(self)
+    self.site_obj.setText(self.remote_obj.get_cooked_body())
+    self.site_obj.reindexObject()
