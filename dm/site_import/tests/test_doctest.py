8121cfd3240b9ab8d531bb2532bfc6df6d06a962
Brian Roberg <robergb@dm.org>
2013-02-19 14:31:38 -0500

    Tests for image suffix removal.

--- dm/site_import/tests/test_doctest.py
+++ dm/site_import/tests/test_doctest.py
@@ -31,6 +31,46 @@ class RemoteObjectTesting(unittest.TestCase):
       remote_obj = RemoteObject('http://www.dm.org/site-homepage')
       self.assertEqual(remote_obj.absolute_url, 'http://www.dm.org/site-homepage')
 
+    def test_get_image_url_large(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_large'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_preview(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_preview'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_mini(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_mini'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_thumb(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_thumb'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_tile(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_tile'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_icon(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_icon'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_listing(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/image_listing'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_trailing_slash(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg/'), 'http://www.dm.org/foo.jpg')
+
+    def test_get_image_url_no_change(self):
+      remote_obj = RemoteObject('http://www.dm.org/site-homepage')
+      self.assertEqual(remote_obj.get_image_url('http://www.dm.org/foo.jpg'), 'http://www.dm.org/foo.jpg')
+
+
+     
+      
+
     def test_is_valid_url(self):
       remote_obj = RemoteObject('http://www.dm.org/site-homepage')
       self.assertTrue(remote_obj.is_valid_url('http://www.dm.org'))
