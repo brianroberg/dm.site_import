4106d59f5af99650f53543d3e4bed260a0584a64
Brian Roberg <robergb@dm.org>
2013-02-27 15:02:28 -0500

    Retrieve shortname using relative_url instead of calling title_and_id.

--- dm/site_import/browser/RemoteObject.py
+++ dm/site_import/browser/RemoteObject.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import httplib
 import re
+import urllib
 import urlparse
 
 class HTTPError(Exception):
@@ -119,15 +120,8 @@ class RemoteObject(RemoteResource):
     self.relative_url = relative_url_str.split('/')
 
     # Retrieve the title and id (which we'll call "shortname")
-    title_and_id = self.make_http_request('title_and_id')
-    match = re.match('(.*)(\s+)\((.*)\)', title_and_id)
-    if match:
-      self.title = match.group(1).strip()
-      self.shortname = match.group(3).strip()
-    else:
-      print "No regexp match with title_and_id = '%s'" % title_and_id
-      self.shortname = self.title = title_and_id
-    
+    self.title = self.make_http_request('Title')
+    self.shortname = urllib.unquote(self.relative_url[-1])
     self.obj_type = self.make_http_request('Type')
     # Sometimes calling /Type on images returns "Plone Site," so
     # if that's what came back, double-check.
