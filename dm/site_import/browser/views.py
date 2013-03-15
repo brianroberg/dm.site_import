1711ac4aa7a2ef91915f0504d17d858b7cf54e89
Brian Roberg <robergb@dm.org>
2013-03-15 15:53:56 -0400

    Don't import sitemaps; fixed indentation on several lines.

--- dm/site_import/browser/views.py
+++ dm/site_import/browser/views.py
@@ -11,9 +11,9 @@ class DMSiteImportView(BrowserView):
   def __call__(self):
     self.remove_events_and_news()
 
-    starting_url = 'http://gettysburg.dm.org/site-homepage'
-    #starting_url = 'https://staff.dm.org/index_html'
-
+    #starting_url = 'http://www.dm.org'
+    #starting_url = 'http://gettysburg.dm.org/audio-archive/fall-2012-jesus-is-better-matthews-gospel'
+    starting_url = 'https://staff.dm.org'
 
     crawler = Crawler(starting_url)
     import_objects = crawler.get_import_objects()
@@ -24,17 +24,21 @@ class DMSiteImportView(BrowserView):
     for url in urls:
       remote_obj = import_objects[url]
       if remote_obj.obj_type == 'Page':
-	import_obj = ImportPage(remote_obj, self)
-	import_obj.create()
+        # The sitemap is a special case: it's helpful to crawl
+        # it, but we don't want to import it.
+        if remote_obj.shortname == 'sitemap':
+          continue
+        import_obj = ImportPage(remote_obj, self)
+        import_obj.create()
       elif remote_obj.obj_type == 'Folder':
-	import_obj = ImportFolder(remote_obj, self)
-	import_obj.create()
+        import_obj = ImportFolder(remote_obj, self)
+        import_obj.create()
       elif remote_obj.obj_type == 'Image':
-	import_obj = ImportImage(remote_obj, self)
-	import_obj.create()
+        import_obj = ImportImage(remote_obj, self)
+        import_obj.create()
       elif remote_obj.obj_type == 'File':
-	import_obj = ImportFile(remote_obj, self)
-	import_obj.create()
+        import_obj = ImportFile(remote_obj, self)
+        import_obj.create()
 
 
 
